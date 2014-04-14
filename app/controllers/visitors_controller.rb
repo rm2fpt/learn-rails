6a926bb0af980203721a5aa6f3375d16b7cd80ed
ubuntu <ubuntu@ubuntupc.(none)>
2014-04-14 21:05:25 +0100

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,5 +3,9 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 		#render 'visitors/new'
+		flash.now[:notice] = 'Bem-vindo!'
+		flash.now[:alert] = 'O meu aniversário está para breve.'
+
+
 	end
 end
