98ce852caadb863559851c1dc1cc8df62a35542b
ubuntu <ubuntu@ubuntupc.(none)>
2014-04-15 12:10:18 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,10 +2,5 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		#render 'visitors/new'
-		flash.now[:notice] = 'Bem-vindo!'
-		flash.now[:alert] = 'O meu aniversário está para breve.'
-
-
 	end
 end
